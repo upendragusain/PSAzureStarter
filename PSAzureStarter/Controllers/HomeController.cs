4fe529683e34035f4e9db1c81029b6d2e82b1304
Upendra Gusain <upendra.gusain@gmail.com>
2020-04-18 13:26:08 +0100

    add config setting for greeting

--- PSAzureStarter/Controllers/HomeController.cs
+++ PSAzureStarter/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using PSAzureStarter.Models;
 
@@ -12,15 +13,18 @@ namespace PSAzureStarter.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index", _configuration["Greeting"]);
         }
 
         public IActionResult Privacy()
